4a3b0bb13e2e0a9b8e62ec30b68abce8296000c0
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-14 22:59:04 +0200

    try json

--- backend.js
+++ backend.js
@@ -7,7 +7,13 @@ button.addEventListener('click', async function () {
         let response = await fetch("https://misterd05.github.io/JurassicPark/trexInfo.json").then(function (response) { return response.json() });
 
         console.log(response);
-        document. getElementById("Trex").innerHTML= (response);
+        document. getElementById("Trex").innerHTML= ("<h2>Nome: " + response.name + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>scientificName: " + response.scientificName + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>type: " + response.type + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>diet: " + response.diet + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>length: " + response.length + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>height: " + response.height + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>weight: " + response.weight + "</h2>");
     }
 })
 
