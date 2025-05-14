02d29d2270451405c45b5cab27d15c9287aad125
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-14 23:17:12 +0200

    try visual info

--- backend.js
+++ backend.js
@@ -2,19 +2,21 @@ let button = document.getElementById("trex");
 let visible = false;
 button.addEventListener('click', async function () {
     if(visible){
-        document. getElementById("Trex").innerHTML = "";
+        document.getElementById("title").innerHTML = ("<h1>Tirannosaurus Rex</h1>")
+        document. getElementById("Trextext").innerHTML = ("<div></div>");
         visible = !visible;
     }else{
         let response = await fetch("https://misterd05.github.io/JurassicPark/trexInfo.json").then(function (response) { return response.json() });
 
         console.log(response);
-        document. getElementById("Trex").innerHTML= ("<h2>Nome: " + response.name + "</h2>");
-        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>scientificName: ' + response.scientificName + '</h2>');
-        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>type: ' + response.type + '</h2>');
-        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>diet: ' + response.diet + '</h2>');
-        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>length: ' + response.length + '</h2>');
-        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>height: ' + response.height + '</h2>');
-        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>weight: ' + response.weight + '</h2>');
+        document.getElementById("title").innerHTML = ("<img src='../img/mrDNA.png' alt='' height='60px' width='60px'>")
+        document. getElementById("Trextext").innerHTML= ("<h2>Nome: " + response.name + "</h2>");
+        document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>scientificName: ' + response.scientificName + '</h2>');
+        document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>type: ' + response.type + '</h2>');
+        document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>diet: ' + response.diet + '</h2>');
+        document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>length: ' + response.length + '</h2>');
+        document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>height: ' + response.height + '</h2>');
+        document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>weight: ' + response.weight + '</h2>');
         visible = !visible;
     }
 })
