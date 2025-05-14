9ef346f6400c50a37c54ee4af31bec8ea3ad56b1
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-14 23:19:41 +0200

    visual effects

--- backend.js
+++ backend.js
@@ -9,7 +9,7 @@ button.addEventListener('click', async function () {
         let response = await fetch("https://misterd05.github.io/JurassicPark/trexInfo.json").then(function (response) { return response.json() });
 
         console.log(response);
-        document.getElementById("title").innerHTML = ("<img src='../img/mrDNA.png' alt='' height='60px' width='60px'>")
+        document.getElementById("title").innerHTML = ("<img src='../img/trexInfo.png' alt='' height='60px' width='60px'>")
         document. getElementById("Trextext").innerHTML= ("<h2>Nome: " + response.name + "</h2>");
         document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>scientificName: ' + response.scientificName + '</h2>');
         document. getElementById("Trextext").insertAdjacentHTML('beforeend', '<h2>type: ' + response.type + '</h2>');
