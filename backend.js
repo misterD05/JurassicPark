f6660336a7e59bfe480507dec65440e71573af56
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-14 23:04:28 +0200

    Json sistemato e style

--- backend.js
+++ backend.js
@@ -3,17 +3,19 @@ let visible = false;
 button.addEventListener('click', async function () {
     if(visible){
         document. getElementById("Trex").innerHTML = "";
+        visible = !visible;
     }else{
         let response = await fetch("https://misterd05.github.io/JurassicPark/trexInfo.json").then(function (response) { return response.json() });
 
         console.log(response);
-        document. getElementById("Trex").innerHTML= ("<h2>Nome: " + response.name + "</h2>");
-        document. getElementById("Trex").innerHTML= ("<h2>scientificName: " + response.scientificName + "</h2>");
-        document. getElementById("Trex").innerHTML= ("<h2>type: " + response.type + "</h2>");
-        document. getElementById("Trex").innerHTML= ("<h2>diet: " + response.diet + "</h2>");
-        document. getElementById("Trex").innerHTML= ("<h2>length: " + response.length + "</h2>");
-        document. getElementById("Trex").innerHTML= ("<h2>height: " + response.height + "</h2>");
-        document. getElementById("Trex").innerHTML= ("<h2>weight: " + response.weight + "</h2>");
+        document. getElementById("Trex").write("<h2>Nome: " + response.name + "</h2>");
+        document. getElementById("Trex").write("<h2>scientificName: " + response.scientificName + "</h2>");
+        document. getElementById("Trex").write("<h2>type: " + response.type + "</h2>");
+        document. getElementById("Trex").write("<h2>diet: " + response.diet + "</h2>");
+        document. getElementById("Trex").write("<h2>length: " + response.length + "</h2>");
+        document. getElementById("Trex").write("<h2>height: " + response.height + "</h2>");
+        document. getElementById("Trex").write("<h2>weight: " + response.weight + "</h2>");
+        visible = !visible;
     }
 })
 
