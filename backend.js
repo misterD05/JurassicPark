027f607ddbc43574041d8e47a2bf5bd184097a55
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-05-14 23:08:34 +0200

    try print json

--- backend.js
+++ backend.js
@@ -8,13 +8,13 @@ button.addEventListener('click', async function () {
         let response = await fetch("https://misterd05.github.io/JurassicPark/trexInfo.json").then(function (response) { return response.json() });
 
         console.log(response);
-        document. getElementById("Trex").write("<h2>Nome: " + response.name + "</h2>");
-        document. getElementById("Trex").write("<h2>scientificName: " + response.scientificName + "</h2>");
-        document. getElementById("Trex").write("<h2>type: " + response.type + "</h2>");
-        document. getElementById("Trex").write("<h2>diet: " + response.diet + "</h2>");
-        document. getElementById("Trex").write("<h2>length: " + response.length + "</h2>");
-        document. getElementById("Trex").write("<h2>height: " + response.height + "</h2>");
-        document. getElementById("Trex").write("<h2>weight: " + response.weight + "</h2>");
+        document. getElementById("Trex").innerHTML= ("<h2>Nome: " + response.name + "</h2>");
+        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>scientificName: ' + response.scientificName + '</h2>');
+        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>type: ' + response.type + '</h2>');
+        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>diet: ' + response.diet + '</h2>');
+        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>length: ' + response.length + '</h2>');
+        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>height: ' + response.height + '</h2>');
+        document. getElementById("Trex").insertAdjacentHTML('beforeend', '<h2>weight: ' + response.weight + '</h2>');
         visible = !visible;
     }
 })
