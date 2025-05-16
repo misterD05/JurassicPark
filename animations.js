ae0287f0f9143efdf8985e1d04ec6a76a0f3ab09
seba <sebastiano.michi@gmail.com>
2025-05-16 13:38:08 +0200

    Final dinosaur

--- animations.js
+++ animations.js
@@ -2,13 +2,10 @@ var containers = document.getElementsByClassName('DinoContainer');
 var drones = document.getElementsByClassName('drone');
 
 
-for (let container of containers) {
-    for(let drone of drones){
-        container.addEventListener("mousemove", function(event) {
-            drone.style.transform = `translate3d(${(event.offsetX)}px, ${(event.offsetY )}px, 0)`;
-        });
-    }
-
+for (let i = 0; i < containers.length; i++) {
+    containers[i].addEventListener("mousemove", function(event) {
+            drones[i].style.transform = `translate3d(${(event.offsetX)}px, ${(event.offsetY )}px, 0)`;
+    });
 }
 
 
